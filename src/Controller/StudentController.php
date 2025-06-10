8002b01d2af8e7022d82d3457fa0f74b3d8131c2
Ariel Bensing <arielbensing1@gmail.com>
2025-06-11 03:37:19 +0800

    Endroid 2 errors

---
+++ src/Controller/StudentController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Student;
+use App\Repository\StudentRepository;
+use App\Form\StudentTypeForm as StudentType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Uid\Uuid;
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Writer\PngWriter;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
+
+
+final class StudentController extends AbstractController
+{
+
+    #[Route('/student', name: 'app_student_index')]
+    public function index(StudentRepository $studentRepository): Response
+        {
+            $students = $studentRepository->findAll();
+
+            return $this->render('student/index.html.twig', [
+                'students' => $students,
+            ]);
+        }
+
+    #[Route('/student/new', name: 'app_student_new')]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $student = new Student();
+
+        $form = $this->createForm(StudentType::class, $student);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Generate a UUID string for QR field
+            $student->setQr(Uuid::v4()->toRfc4122());
+
+            $em->persist($student);
+            $em->flush();
+
+            // Redirect to a show page (optional)
+            return $this->redirectToRoute('app_student_show', [
+                'id' => $student->getId(),
+            ]);
+        }
+
+        return $this->render('student/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+        #[Route('/student/{id}/qr', name: 'app_student_qr')]
+            public function generateQr(Student $student): Response
+                {
+                    $qrValue = $student->getQr() ?: 'no-qr-value';
+
+                    // Create QR code with all parameters in constructor:
+                    $qrCode = new QrCode(
+                        $qrValue,
+                        new Encoding('UTF-8'),
+                        new ErrorCorrectionLevelHigh(),
+                        300, // size
+                        10   // margin
+                    );
+
+                    $writer = new PngWriter();
+                    $result = $writer->write($qrCode);
+
+                    return new Response(
+                        $result->getString(),
+                        Response::HTTP_OK,
+                        ['Content-Type' => 'image/png']
+                    );
+                }
+
+ 
+
+        #[Route('/student/{id}', name: 'app_student_show')]
+            public function show(Student $student): Response
+            {
+                return $this->render('student/show.html.twig', [
+                    'student' => $student,
+                ]);
+            }
+}

f7d7335aadfdb9d2656ea3b1f5e6c769fe96042b
Souri-Dev <arielbensing1@gmail.com>
2025-07-03 04:08:04 +0800

    Adding Edit Function on Subject

--- src/Controller/SubjectController.php
+++ src/Controller/SubjectController.php
@@ -27,14 +27,57 @@ final class SubjectController extends AbstractController
             $em->flush();
 
             // redirect or render as needed
-            return $this->redirectToRoute('app_subject', [
-                // 'id' => $student->getId(),
-            ]);
+            return $this->redirectToRoute('app_subject', []);
         }
+
+        $editForms = [];
+        foreach ($schoolClasses as $s) {
+            $editForms[$s->getId()] = $this->createForm(SchoolClassTypeForm::class, $s, [
+                'action' => $this->generateUrl('app_subject_edit', ['id' => $s->getId()]),
+                'method' => 'POST',
+            ])->createView();
+        }
+
         return $this->render('subject/index.html.twig', [
             'controller_name' => 'SubjectController',
             'form' => $form->createView(),
             'schoolClasses' => $schoolClasses,
+            'editForms' => $editForms,
         ]);
+    }
+
+    #[Route('/subject/{id}/edit', name: 'app_subject_edit', methods: ['POST'])]
+    public function edit(Request $request, SchoolClass $schoolClass, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(SchoolClassTypeForm::class, $schoolClass, [
+            'action' => $this->generateUrl('app_subject_edit', ['id' => $schoolClass->getId()]),
+            'method' => 'POST',
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success', 'Subject updated successfully.');
+            return $this->redirectToRoute('app_subject');
+        }
+
+        $schoolClasses = $em->getRepository(SchoolClassTypeForm::class)->findAll();
+        $editForms = [];
+        foreach ($schoolClasses as $s) {
+            $editForms[$s->getId()] = $this->createForm(SchoolClassTypeForm::class, $s, [
+                'action' => $this->generateUrl('app_subject_edit', ['id' => $s->getId()]),
+                'method' => 'POST',
+            ]);
+            $editForms[$s->getId()] = $form->createView();
+        }
+
+        $editForms[$schoolClass->getId()] = $form->createView();
+        return $this->render('student/index.html.twig', [
+            'schoolClasses' => $schoolClasses,
+            'form' => $form->createView(),
+            'editForms' => $editForms,
+        ]);
+
+        return $this->redirectToRoute('app_subject'); // fallback
     }
 }

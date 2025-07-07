f99ca37bb785bc570576181df81a7739e1812bfc
Souri-Dev <arielbensing1@gmail.com>
2025-07-07 10:57:25 +0800

    Fix database

--- migrations/Version20250707024443.php
+++ migrations/Version20250707024443.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250618153044 extends AbstractMigration
+final class Version20250707024443 extends AbstractMigration
 {
     public function getDescription(): string
     {
